d896764faa1f59d8c5bfdca538b96e52a0162b9b
sam6 <msam04@gmail.com>
2021-02-27 18:14:51 +0530

    Adding analysis by PD

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+import seaborn as sns
+
 
 df = pd.read_csv("raw_data_TSC.csv")
 
@@ -48,28 +50,44 @@ print(pivot_table)
 
 print(type(pivot_table.loc['2014']))
 
-fig, axarr = plt.subplots(3, figsize=(10,8))
-ticks = [50, 100, 150,200,250,300,350,400]
+# fig, axarr = plt.subplots(3, figsize=(10,8))
+# ticks = [50, 100, 150,200,250,300,350,400]
 
-# axarr[0,0].scatter(pivot_table.columns, pivot_table[pivot_table['PD_Year'] == '2014'])
-# axarr[0,0].set_title('2014')
-# axarr[0,1].scatter(pivot_table.columns,pivot_table[pivot_table['PD_Year'] == '2016'])
-# axarr[0,1].set_title('2016')
-axarr[0].plot(pivot_table.columns,pivot_table.loc['2018'], '*m-')
-axarr[0].set_title('2018')
-axarr[1].plot(pivot_table.columns,pivot_table.loc['2019'],'*g-')
-axarr[1].set_title('2019')
-axarr[2].plot(pivot_table.columns,pivot_table.loc['2020'],'*b-')
-axarr[2].set_title('2020')
+# # axarr[0,0].scatter(pivot_table.columns, pivot_table[pivot_table['PD_Year'] == '2014'])
+# # axarr[0,0].set_title('2014')
+# # axarr[0,1].scatter(pivot_table.columns,pivot_table[pivot_table['PD_Year'] == '2016'])
+# # axarr[0,1].set_title('2016')
+# axarr[0].plot(pivot_table.columns,pivot_table.loc['2018'], '*m-')
+# axarr[0].set_title('2018')
+# axarr[1].plot(pivot_table.columns,pivot_table.loc['2019'],'*g-')
+# axarr[1].set_title('2019')
+# axarr[2].plot(pivot_table.columns,pivot_table.loc['2020'],'*b-')
+# axarr[2].set_title('2020')
 
-axarr[0].set_yticks([100,200,300,400, 500])
-axarr[1].set_yticks(ticks)
-axarr[2].set_yticks(ticks)
+# axarr[0].set_yticks([100,200,300,400, 500])
+# axarr[1].set_yticks(ticks)
+# axarr[2].set_yticks(ticks)
 
-# fig.text(0.5, 0.04, 'Priority Date - Month in year', ha='center', va='center')
-fig.text(0.01, 0.5, 'Waiting time in number of days', ha='center', va='center', rotation='vertical')
-fig.tight_layout()
+# # fig.text(0.5, 0.04, 'Priority Date - Month in year', ha='center', va='center')
+# fig.text(0.01, 0.5, 'Waiting time in number of days', ha='center', va='center', rotation='vertical')
+# fig.tight_layout()
 
 # fig.show()
 
-# fig.save("Mean waiting time based on priority date - Month and Year.png")
+
+df1 = df1.rename(columns = {'M': 'Number of days of wait time'})
+
+print(wait_days_per_pd_month.columns)
+
+f, axes = plt.subplots(2)
+# pivot_table.reset_index(inplace=True)
+
+# di = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug",9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec" }
+# df1['PD_Month'] = df1['PD_Month'].map(di)
+
+sns.boxplot(x='PD_Month',y='Number of days of wait time',data=df1[df1['PD_Year']=='2018'] ,ax=axes[0])
+axes[0].set_title('2018')
+sns.boxplot(x='PD_Month',y='Number of days of wait time',data=df1[df1['PD_Year']=='2019'] ,ax=axes[1])
+axes[1].set_title('2019')
+f.tight_layout()
+f.show()

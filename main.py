30b7c892d1d222acedcc5b62273752e665e6844d
msam04 <msam04@users.noreply.github.com>
2021-05-08 18:44:47 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -110,19 +110,70 @@ from collections import Counter
 most_common = Counter(zip(PD_list,Notice_list)).most_common()
 ##print(Counter(zip(PD_list,Notice_list)).most_common())
 
-most_common_ind_sept_2020 = [i for i, val in enumerate(most_common) if val[0][0]=='2019-09']
+most_common_ind_sept_2019 = [i for i, val in enumerate(most_common) if val[0][0]=='2019-09']
+most_common_ind_sept_2020 = [i for i, val in enumerate(most_common) if val[0][0]=='2020-09']
 
-sept_most_common = [most_common[i] for i in most_common_ind_sept_2020]
+sept_most_common = [most_common[i] for i in most_common_ind_sept_2019]
 
 num_cases = sum([val[1] for val in sept_most_common])
 
+print("*"*45)
 print("Total Number of Cases in Sept-2019")
 print("########  "+str(num_cases)+"  #############")
+print("*"*45)
 
-percentage = [(val[1] / num_cases) * 100 for val in sept_most_common]
+percent = [(val[1]/num_cases) * 100 for val in sept_most_common]
 
 print("*"*45)
 for i in range(len(sept_most_common)):
-    print("Percentage cases approved in: {} - {}".format(most_common[i][0][1], int(percentage[i])))
+    print("Percentage cases approved in: {} - {}".format(sept_most_common[i][0][1], int(percent[i])))
 print("*"*45)
+
+sept_most_common_2020 = [most_common[i] for i in most_common_ind_sept_2020]
+
+num_cases = sum([val[1] for val in sept_most_common_2020])
+percent = [(val[1]/num_cases) * 100 for val in sept_most_common_2020]
+
+print("*"*45)
+print("Total Number of Cases in Sept-2020")
+print("########  "+str(num_cases)+"  #############")
+print("*"*45)
+
+print("*"*45)
+for i in range(len(sept_most_common_2020)):
+    print("Percentage cases approved in: {} - {}".format(sept_most_common_2020[i][0][1], int(percent[i])))
+print("*"*45)
+
+fig, axarr = plt.subplots(2, figsize=(10,8))
+ticks = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+
+sept_2019_cols = ['2020-04', '2020-05', '2020-06', '2020-07', '2020-08','2020-09', '2020-10', '2020-11', '2020-12', '2021-01', '2021-02','2021-03','2021-04']
+sept_2019_vals = [0]*len(sept_2019_cols)
+for i in range(len(sept_2019_cols)):
+    for j in range(len(sept_most_common)):
+        if (sept_most_common[j][0][1] == sept_2019_cols[i]):
+            sept_2019_vals[i] = sept_most_common[j][1]
+
+print(sept_2019_vals)
+axarr[0].plot(sept_2019_cols,sept_2019_vals,'*g-')
+axarr[0].set_title('I140 NIW Sept 2019 PD Case Approvals')
+sept_2020_cols = ['2021-04', '2021-05', '2021-06', '2021-07', '2021-08','2021-09', '2021-10', '2021-11', '2021-12', '2022-01', '2022-02','2022-03','2022-04']
+sept_2020_vals = [0]*len(sept_2020_cols)
+for i in range(len(sept_2020_cols)):
+    for j in range(len(sept_most_common_2020)):
+        if (sept_most_common_2020[j][0][1] == sept_2020_cols[i]):
+            sept_2020_vals[i] = sept_most_common_2020[j][1]
+
+print(sept_2020_vals)
+axarr[1].plot(sept_2020_cols,sept_2020_vals,'*b-')
+axarr[1].set_title('I140 NIW Sept 2020 PD Case Approvals')
+axarr[0].set_yticks(ticks)
+axarr[1].set_yticks(ticks)
+fig.text(0.01, 0.5, 'I140 NIW approvals for Sept Month', ha='center', va='center', rotation='vertical')
+fig.tight_layout()
+
+fig.show()
+
+
+
 
